d73e3602763d77c2a45e55608b1e5967356c8cb6
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-03-30 16:32:18 -0300

    carrinhodecompra

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './App.css';
+import Carrinhodecompra from './components/carrinho-de-compra';
+import styled from "styled-components"
 
-export default class LabECommerce extends React.Component() {
-  render(){
+
+
+class App extends React.Component{
+
+
+  render() {
     return(
      
-      <>
+      <div>
       
-      
-      
-    
-      
-      
-      </>
+      <Carrinhodecompra/>
+
+      </div>
 
 
     )
   }
  
 }
-
+export default App
